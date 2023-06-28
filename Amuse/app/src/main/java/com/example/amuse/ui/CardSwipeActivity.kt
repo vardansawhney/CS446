8089154bb62650c3885b17952d4d3b0450a19ef8
Alan Wei <aywei@uwaterloo.ca>
2023-06-27 23:50:01 -0400

    Fixed some errors in card swipe activity and swipe adapter

--- Amuse/app/src/main/java/com/example/amuse/ui/CardSwipeActivity.kt
+++ Amuse/app/src/main/java/com/example/amuse/ui/CardSwipeActivity.kt
@@ -3,11 +3,14 @@ package com.example.amuse.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.amuse.R
+import com.example.amuse.databinding.ActivityCardSwipeBinding
+import com.yalantis.library.Koloda
 
 class CardSwipeActivity : AppCompatActivity() {
     private lateinit var cardList: ArrayList<Int>
     private lateinit var swipeAdapter: SwipeAdapter
     private lateinit var koloda: Koloda
+    private lateinit var binding: ActivityCardSwipeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_swipe)
@@ -16,7 +19,7 @@ class CardSwipeActivity : AppCompatActivity() {
         koloda = binding.root.findViewById(R.id.koloda)
         cardList = ArrayList()
 
-        swipeAdapter = SwipeAdapter(cardList)
-        koloda.setAdapter(adapter)
+        swipeAdapter = SwipeAdapter(this, cardList)
+        koloda.adapter = swipeAdapter
     }
 }
--- Amuse/app/src/main/java/com/example/amuse/ui/SwipeAdapter.kt
+++ Amuse/app/src/main/java/com/example/amuse/ui/SwipeAdapter.kt
@@ -1,17 +1,13 @@
 package com.example.amuse.ui
+import com.example.amuse.R
 import android.content.Context
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
-import kotlinx.android.synthetic.main.item_koloda.view.*
 
-class SwipeAdapter(private val context: Context, val data: ArrayList<Int>?) :
+
+class SwipeAdapter(private val context: Context, val data: ArrayList<Int>) :
     BaseAdapter() {
 
     private val dataList = mutableListOf<Int>()
@@ -35,35 +31,42 @@ class SwipeAdapter(private val context: Context, val data: ArrayList<Int>?) :
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
-        val holder: DataViewHolder
-        var view = convertView
-
-        if (view == null) {
-            view = android.view.LayoutInflater.from(parent.context).inflate(R.layout.item_koloda, parent, false)
-            holder = DataViewHolder(view)
-            view?.tag = holder
+//        val holder: DataViewHolder
+//        var view = convertView
+//
+//        if (view == null) {
+//            view = android.view.LayoutInflater.from(parent.context).inflate(R.layout.item_koloda, parent, false)
+//            holder = DataViewHolder(view)
+//            view?.tag = holder
+//        } else {
+//            holder = view.tag as DataViewHolder
+//        }
+//
+//        holder.bindData(context, getItem(position))
+//
+//        return view
+        val view: View
+        if (convertView == null) {
+            view = LayoutInflater.from(parent.context).inflate(R.layout.item_card, parent, false)
         } else {
-            holder = view.tag as DataViewHolder
+            view = convertView
         }
-
-        holder.bindData(context, getItem(position))
-
         return view
     }
 
-    /**
-     * Static view items holder
-     */
-    class DataViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var picture = view.kolodaImage
-
-        internal fun bindData(context: Context, data: Int) {
-            val transforms = RequestOptions().transforms(CenterCrop(), RoundedCorners(20))
-            Glide.with(context)
-                .load(data)
-                .apply(transforms)
-                .into(picture)
-        }
-
-    }
+//    /**
+//     * Static view items holder
+//     */
+//    class DataViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+//        var picture = view.kolodaImage
+//
+//        internal fun bindData(context: Context, data: Int) {
+//            val transforms = RequestOptions().transforms(CenterCrop(), RoundedCorners(20))
+//            Glide.with(context)
+//                .load(data)
+//                .apply(transforms)
+//                .into(picture)
+//        }
+//
+//    }
 }
