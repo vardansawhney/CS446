6be8013ce096dee2888fc3d66398835bb2448219
Alan Wei <aywei@uwaterloo.ca>
2023-06-28 00:56:36 -0400

    Added some changes to CardAdapter and CardSwipeActivity

--- Amuse/app/src/main/java/com/example/amuse/ui/CardAdapter.kt
+++ Amuse/app/src/main/java/com/example/amuse/ui/CardAdapter.kt
@@ -1,4 +1,46 @@
 package com.example.amuse.ui
 
-class CardAdapter {
+import com.example.amuse.R
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.amuse.ui.home.Card
+
+
+class CardAdapter(private val cardsList: ArrayList<Card>) :
+    RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
+
+    class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val imageView : ImageView = itemView. findViewById(R.id.OuterCardMedia)
+        val titleTextView : TextView = itemView.findViewById(R.id.OuterCardTitle)
+        val locationTextView : TextView = itemView.findViewById(R.id.OuterCardLocation)
+        val starsTextView : TextView = itemView.findViewById(R.id.OuterCardStars)
+        val priceTextView : TextView = itemView.findViewById(R.id.OuterCardPrice)
+        val groupsTextView : TextView = itemView.findViewById(R.id.OuterCardGroups)
+        val likesTextView : TextView = itemView.findViewById(R.id.OuterCardLikes)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view: View = inflater.inflate(R.layout.item_card, parent, false)
+        return CardViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
+        val card = cardsList[position]
+        holder.imageView.setImageResource(card.imageId)
+        holder.titleTextView.text = card.title
+        holder.locationTextView.text = card.location
+        holder.starsTextView.text = card.stars
+        holder.priceTextView.text = card.price
+        holder.groupsTextView.text = card.groups
+        holder.likesTextView.text = card.likes
+
+    }
+
+    override fun getItemCount(): Int {
+        return cardsList.size
+    }
 }
--- Amuse/app/src/main/java/com/example/amuse/ui/CardSwipeActivity.kt
+++ Amuse/app/src/main/java/com/example/amuse/ui/CardSwipeActivity.kt
@@ -2,24 +2,37 @@ package com.example.amuse.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.amuse.R
 import com.example.amuse.databinding.ActivityCardSwipeBinding
-import com.yalantis.library.Koloda
+import com.example.amuse.ui.home.Card
+import com.yuyakaido.android.cardstackview.CardStackView
 
 class CardSwipeActivity : AppCompatActivity() {
-    private lateinit var cardList: ArrayList<Int>
-    private lateinit var swipeAdapter: SwipeAdapter
-    private lateinit var koloda: Koloda
+    private lateinit var cardList: ArrayList<Card>
+    private lateinit var cardAdapter: CardAdapter
+    private lateinit var cardStackView: CardStackView
     private lateinit var binding: ActivityCardSwipeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_swipe)
 
         binding = ActivityCardSwipeBinding.inflate(layoutInflater)
-        koloda = binding.root.findViewById(R.id.koloda)
+        cardStackView = binding.root.findViewById(R.id.card_stack)
         cardList = ArrayList()
 
-        swipeAdapter = SwipeAdapter(this, cardList)
-        koloda.adapter = swipeAdapter
+        // Card 1
+        cardList.add(Card("Hiking", "Laurel Trail (2.1 km)", "★★★☆☆", "$", "2 groups", "100 interested", R.drawable.card1_media))
+
+        // Card 2
+        cardList.add(Card("Fancy Dinner", "Shinwa (1.8 km)", "★★★★★", "$$", "1 groups", "2 interested", R.drawable.card2_media))
+
+        // Card 3
+        cardList.add(Card("Clubbing", "Allure (2 km)", "★☆☆☆☆", "$$$$", "0 groups", "0 interested", R.drawable.card3_media))
+
+        cardAdapter = CardAdapter(cardList)
+        cardStackView.adapter = cardAdapter
     }
 }
