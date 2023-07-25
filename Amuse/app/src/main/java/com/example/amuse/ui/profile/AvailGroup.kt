507ededab56fdd9091c5f3bf206633c2428f999a
Denis Tatar <denistatar@v1040-wn-rt-a-68-185.campus-dynamic.uwaterloo.ca>
2023-07-25 17:23:54 -0400

    dynamically join groups UI completed

--- Amuse/app/src/main/java/com/example/amuse/ui/profile/AvailGroup.kt
+++ Amuse/app/src/main/java/com/example/amuse/ui/profile/AvailGroup.kt
@@ -1,9 +1,3 @@
 package com.example.amuse.ui.profile
 
-data class AvailGroup(var name: String, var startTime: String, var endTime: String, var date: String, var availSpotsLeft: Long)
-
-//data class AvailGroup(var name: String, var startTime: String, var endTime: String, var date: String,
-//                      var minMembers: Int, var maxMembers: Int, var accepted: ArrayList<Friend>, var availSpotsLeft: Int)
-
-//data class Group(var name: String, var minMembers: Int, var maxMembers: Int,
-//                 var solo: Boolean, var invited: ArrayList<Friend>, var accepted: ArrayList<Friend>)
+data class AvailGroup(var name: String, var startTime: String, var endTime: String, var date: String, var availSpotsLeft: Long, var buttonID: String)
